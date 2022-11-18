8c0dbaea4cfad7cd8fcff137a7605c4a735c4161
Atelok <diego135tc@gmail.com>
2022-11-17 23:50:37 -0500

    listo

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -202,16 +202,15 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  var igual = arreglo[0];
-  
-  for (var i = 1; i < arreglo.length; i++) {
-    if (igual === arreglo[i]) {
 
+  
+  for (var i = 0; i < arreglo.length - 1; i++) {
+    if (arreglo[i] !== arreglo[i+1]) {
+      return false;
     }
-    return true;
   }
   
-  return false;
+  return true;
 } 
 
 
@@ -220,13 +219,37 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
-}
+  var newarray = [];
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === "Enero"|| array[i] === "Noviembre" || array[i] === "Marzo") {
+      newarray.push(array[i]);
+    }
+  }
+
+  if (newarray.length < 3) {
+    return "No se encontraron los meses pedidos";
+  } 
+  else {
+    return newarray;
+     }
+  }
+ 
 
 
 function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+var newarray = [];
+
+for (var i = 0; i < array.length; i++) {
+
+  if (array[i] > 100) {
+    newarray.push(array[i])
+  }
+}
+return newarray;
+
 }
 
 
@@ -238,7 +261,25 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
-}
+  var newarray = [];
+  var limite = numero;
+ 
+  for (var i = 0; i < 10; i++) {
+   
+    limite = limite + 2;
+
+    if (limite === i) { break;}
+
+    newarray.push(limite); 
+  }
+
+    if (newarray.length < 10) {
+      return "Se interrumpió la ejecución";
+     }
+     else{
+      return newarray;
+     }
+  }
 
 
 function continueStatement(numero) {
@@ -248,7 +289,22 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  var newarray = [];
+  var limite = numero;
+ 
+  for (var i = 0; i < 10; i++) {
+   
+    if (i === 5) { continue; }
+    else  {  
+      limite = limite + 2;
+      newarray.push(limite); 
+          }
+  
+  }
+
+return newarray;
 }
+
 
 
 // No modificar nada debajo de esta línea
